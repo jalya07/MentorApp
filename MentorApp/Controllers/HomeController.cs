7c944947f5a13017249956d914b1432abc3a064f
jalya07 <jalavh@code.edu.az>
2026-04-02 22:57:34 +0400

    change

--- MentorApp/Controllers/HomeController.cs
+++ MentorApp/Controllers/HomeController.cs
@@ -2,15 +2,21 @@ using System.Diagnostics;
 using MentorApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using MentorApp.Models;
+using MentorApp.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace MentorApp.Controllers;
 
 public class HomeController(MentorAppDbContext context) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        var sliders = context.Sliders.ToList();
-        return View(sliders);
+        var slider = await context.Sliders.FirstOrDefaultAsync();
+        HomeVm homeVm = new HomeVm
+        {
+            Slider = slider
+        };
+        return View(homeVm);
     }
 
     
--- MentorApp/Models/ViewModels/HomeVm.cs
+++ MentorApp/Models/ViewModels/HomeVm.cs
@@ -2,5 +2,5 @@ namespace MentorApp.Models.ViewModels;
 
 public class HomeVm
 {
-    
+        public Slider Slider { get; set; }= null!;
 }
